2faa04e2931e4b1e62f73479025e81d4465cced5
wswmsword <cjzjzzz@gmail.com>
2023-02-17 21:15:57 +0800

    add px-to-viewport tests to support partial px-to-viewport ability

--- spec/mobile-to-multi-displays.spec.js
+++ spec/mobile-to-multi-displays.spec.js
@@ -268,3 +268,326 @@ describe("include and exclude", function() {
     expect(processed).toBe(converted);
   });
 });
+
+describe('px-to-viewport', function() {
+  var basicCSS = '.rule { font-size: 15px }';
+  var basicOptions = {
+    viewportWidth: 320,
+    disableDesktop: true,
+    disableLandscape: true,
+    enableMobile: true,
+    unitPrecision: 5,
+  };
+  it('should work on the readme example', function () {
+    var input = 'h1 { margin: 0 0 20px; font-size: 32px; line-height: 2; letter-spacing: 1px; }';
+    var output = 'h1 { margin: 0 0 6.25vw; font-size: 10vw; line-height: 2; letter-spacing: 1px; }';
+    var processed = postcss(mobileToMultiDisplays(basicOptions)).process(input).css;
+
+    expect(processed).toBe(output);
+  });
+
+  it('should replace the px unit with vw', function () {
+    var processed = postcss(mobileToMultiDisplays(basicOptions)).process(basicCSS).css;
+    var expected = '.rule { font-size: 4.6875vw }';
+
+    expect(processed).toBe(expected);
+  });
+
+  it('should handle < 1 values and values without a leading 0', function () {
+    var rules = '.rule { margin: 0.5rem .5px -0.2px -.2em }';
+    var expected = '.rule { margin: 0.5rem 0.15625vw -0.0625vw -.2em }';
+    var processed = postcss(mobileToMultiDisplays(basicOptions)).process(rules).css;
+
+    expect(processed).toBe(expected);
+  });
+
+  it('should remain unitless if 0', function () {
+    var expected = '.rule { font-size: 0px; font-size: 0; }';
+    var processed = postcss(mobileToMultiDisplays(basicOptions)).process(expected).css;
+
+    expect(processed).toBe(expected);
+  });
+
+  // it('should not add properties that already exist', function () {
+  //   var expected = '.rule { font-size: 16px; font-size: 5vw; }';
+  //   var processed = postcss(mobileToMultiDisplays(basicOptions)).process(expected).css;
+
+  //   expect(processed).toBe(expected);
+  // });
+
+  it('should not replace units inside mediaQueries by default', function() {
+    var expected = '@media (min-width: 500px) { .rule { font-size: 16px } }';
+    var processed = postcss(mobileToMultiDisplays(basicOptions)).process('@media (min-width: 500px) { .rule { font-size: 16px } }').css;
+
+    expect(processed).toBe(expected);
+  })
+
+  describe('value parsing', function() {
+    it('should not replace values in double quotes or single quotes', function () {
+      var rules = '.rule { content: \'16px\'; font-family: "16px"; font-size: 16px; }';
+      var expected = '.rule { content: \'16px\'; font-family: "16px"; font-size: 5vw; }';
+      var processed = postcss(mobileToMultiDisplays(basicOptions)).process(rules).css;
+  
+      expect(processed).toBe(expected);
+    });
+  
+    it('should not replace values in `url()`', function () {
+      var rules = '.rule { background: url(16px.jpg); font-size: 16px; }';
+      var expected = '.rule { background: url(16px.jpg); font-size: 5vw; }';
+      var processed = postcss(mobileToMultiDisplays(basicOptions)).process(rules).css;
+  
+      expect(processed).toBe(expected);
+    });
+  
+    it('should not replace values with an uppercase P or X', function () {
+      var rules = '.rule { margin: 12px calc(100% - 14PX); height: calc(100% - 20px); font-size: 12Px; line-height: 16px; }';
+      var expected = '.rule { margin: 3.75vw calc(100% - 14PX); height: calc(100% - 6.25vw); font-size: 12Px; line-height: 5vw; }';
+      var processed = postcss(mobileToMultiDisplays(basicOptions)).process(rules).css;
+  
+      expect(processed).toBe(expected);
+    });
+  });
+
+
+  describe('viewportWidth', function() {
+    it('should should replace using 320px by default', function() {
+      var basicOptions = {
+        viewportWidth: 320,
+        disableDesktop: true,
+        disableLandscape: true,
+        enableMobile: true,
+        unitPrecision: 5,
+      };
+      var expected = '.rule { font-size: 4.6875vw }';
+      var processed = postcss(mobileToMultiDisplays(basicOptions)).process(basicCSS).css;
+  
+      expect(processed).toBe(expected);
+    });
+  
+    it('should replace using viewportWidth from options', function() {
+      var basicOptions = {
+        viewportWidth: 480,
+        disableDesktop: true,
+        disableLandscape: true,
+        enableMobile: true,
+        unitPrecision: 5,
+      };
+      var expected = '.rule { font-size: 3.125vw }';
+      var processed = postcss(mobileToMultiDisplays(basicOptions)).process(basicCSS).css;
+  
+      expect(processed).toBe(expected);
+    })
+  });
+
+
+  describe('unitPrecision', function () {
+    it('should replace using a decimal of 2 places', function () {
+      var expected = '.rule { font-size: 4.69vw }';
+      var processed = postcss(mobileToMultiDisplays({
+        ...basicOptions,
+        unitPrecision: 2
+      })).process(basicCSS).css;
+  
+      expect(processed).toBe(expected);
+    });
+  });
+
+  describe('exclude', function () {
+    var rules = '.rule { border: 1px solid #000; font-size: 16px; margin: 1px 10px; }';
+    var covered = '.rule { border: 1px solid #000; font-size: 5vw; margin: 1px 3.125vw; }';
+    it('when using regex at the time, the style should not be overwritten.', function () {
+      var processed = postcss(mobileToMultiDisplays({
+        ...basicOptions,
+        exclude: /\/node_modules\//
+      })).process(rules, {
+        from: '/node_modules/main.css'
+      }).css;
+  
+      expect(processed).toBe(rules);
+    });
+  
+    it('when using regex at the time, the style should be overwritten.', function () {
+      var processed = postcss(mobileToMultiDisplays({
+        ...basicOptions,
+        exclude: /\/node_modules\//
+      })).process(rules, {
+        from: '/example/main.css'
+      }).css;
+  
+      expect(processed).toBe(covered);
+    });
+  
+    it('when using array at the time, the style should not be overwritten.', function () {
+      var processed = postcss(mobileToMultiDisplays({
+        ...basicOptions,
+        exclude: [/\/node_modules\//, /\/exclude\//]
+      })).process(rules, {
+        from: '/exclude/main.css'
+      }).css;
+  
+      expect(processed).toBe(rules);
+    });
+  
+    it('when using array at the time, the style should be overwritten.', function () {
+      var processed = postcss(mobileToMultiDisplays({
+        ...basicOptions,
+        exclude: [/\/node_modules\//, /\/exclude\//]
+      })).process(rules, {
+        from: '/example/main.css'
+      }).css;
+  
+      expect(processed).toBe(covered);
+    });
+  });
+  
+  describe('include', function () {
+    var rules = '.rule { border: 1px solid #000; font-size: 16px; margin: 1px 10px; }';
+    var covered = '.rule { border: 1px solid #000; font-size: 5vw; margin: 1px 3.125vw; }';
+    it('when using regex at the time, the style should not be overwritten.', function () {
+      var processed = postcss(mobileToMultiDisplays({
+        ...basicOptions,
+        include: /\/mobile\//
+      })).process(rules, {
+        from: '/pc/main.css'
+      }).css;
+  
+      expect(processed).toBe(rules);
+    });
+  
+    it('when using regex at the time, the style should be overwritten.', function () {
+      var processed = postcss(mobileToMultiDisplays({
+        ...basicOptions,
+        include: /\/mobile\//,
+      })).process(rules, {
+        from: '/mobile/main.css'
+      }).css;
+  
+      expect(processed).toBe(covered);
+    });
+  
+    it('when using array at the time, the style should not be overwritten.', function () {
+      var processed = postcss(mobileToMultiDisplays({
+        ...basicOptions,
+        include: [/\/flexible\//, /\/mobile\//]
+      })).process(rules, {
+        from: '/pc/main.css'
+      }).css;
+  
+      expect(processed).toBe(rules);
+    });
+  
+    it('when using array at the time, the style should be overwritten.', function () {
+      var processed = postcss(mobileToMultiDisplays({
+        ...basicOptions,
+        include: [/\/flexible\//, /\/mobile\//]
+      })).process(rules, {
+        from: '/flexible/main.css'
+      }).css;
+  
+      expect(processed).toBe(covered);
+    });
+  });
+  
+  describe('include-and-exclude', function () {
+    var rules = '.rule { border: 1px solid #000; font-size: 16px; margin: 1px 10px; }';
+    var covered = '.rule { border: 1px solid #000; font-size: 5vw; margin: 1px 3.125vw; }';
+  
+    it('when using regex at the time, the style should not be overwritten.', function () {
+      var processed = postcss(mobileToMultiDisplays({
+        ...basicOptions,
+        include: /\/mobile\//,
+        exclude: /\/not-transform\//
+      })).process(rules, {
+        from: '/mobile/not-transform/main.css'
+      }).css;
+  
+      expect(processed).toBe(rules);
+    });
+  
+    it('when using regex at the time, the style should be overwritten.', function () {
+      var processed = postcss(mobileToMultiDisplays({
+        ...basicOptions,
+        include: /\/mobile\//,
+        exclude: /\/not-transform\//
+      })).process(rules, {
+        from: '/mobile/style/main.css'
+      }).css;
+  
+      expect(processed).toBe(covered);
+    });
+  
+    it('when using array at the time, the style should not be overwritten.', function () {
+      var processed = postcss(mobileToMultiDisplays({
+        ...basicOptions,
+        include: [/\/flexible\//, /\/mobile\//],
+        exclude: [/\/not-transform\//, /pc/]
+      })).process(rules, {
+        from: '/flexible/not-transform/main.css'
+      }).css;
+  
+      expect(processed).toBe(rules);
+    });
+  
+    it('when using regex at the time, the style should be overwritten.', function () {
+      var processed = postcss(mobileToMultiDisplays({
+        ...basicOptions,
+        include: [/\/flexible\//, /\/mobile\//],
+        exclude: [/\/not-transform\//, /pc/]
+      })).process(rules, {
+        from: '/mobile/style/main.css'
+      }).css;
+  
+      expect(processed).toBe(covered);
+    });
+  });
+  
+  describe('regex', function () {
+    var rules = '.rule { border: 1px solid #000; font-size: 16px; margin: 1px 10px; }';
+    var covered = '.rule { border: 1px solid #000; font-size: 5vw; margin: 1px 3.125vw; }';
+  
+    it('when using regex at the time, the style should not be overwritten.', function () {
+      var processed = postcss(mobileToMultiDisplays({
+        ...basicOptions,
+        exclude: /pc/
+      })).process(rules, {
+        from: '/pc-project/main.css'
+      }).css;
+  
+      expect(processed).toBe(rules);
+    });
+  
+    it('when using regex at the time, the style should be overwritten.', function () {
+      var processed = postcss(mobileToMultiDisplays({
+        ...basicOptions,
+        exclude: /\/pc\//
+      })).process(rules, {
+        from: '/pc-project/main.css'
+      }).css;
+  
+      expect(processed).toBe(covered);
+    });
+  
+    it('when using regex at the time, the style should not be overwritten.', function () {
+      var processed = postcss(mobileToMultiDisplays({
+        ...basicOptions,
+        include: /\/pc\//
+      })).process(rules, {
+        from: '/pc-project/main.css'
+      }).css;
+  
+      expect(processed).toBe(rules);
+    });
+  
+    it('when using regex at the time, the style should be overwritten.', function () {
+      var processed = postcss(mobileToMultiDisplays({
+        ...basicOptions,
+        include: /pc/
+      })).process(rules, {
+        from: '/pc-project/main.css'
+      }).css;
+  
+      expect(processed).toBe(covered);
+    });
+  });
+
+});
