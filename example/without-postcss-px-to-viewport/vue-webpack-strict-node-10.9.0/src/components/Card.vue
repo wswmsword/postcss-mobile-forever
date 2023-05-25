7fadfbe46aeb41214f7ca15014b083e4b3ec8b61
wswmsword <cjzjzzz@gmail.com>
2023-05-25 13:32:36 +0800

    update example

---
+++ example/without-postcss-px-to-viewport/vue-webpack-strict-node-10.9.0/src/components/Card.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="card">
+    <div class="content"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Card",
+}
+</script>
+
+<style scoped>
+.card {
+  width: 192px;
+  height: 344px;
+  border-radius: 8px;
+  background-color: rgb(219, 191, 139);
+  padding-top: 10px;
+  box-sizing: border-box;
+}
+
+.content {
+  width: 180px;
+  height: 290px;
+  border-radius: 8px;
+  background-color: black;
+  margin: 0 auto;
+}
+</style>
