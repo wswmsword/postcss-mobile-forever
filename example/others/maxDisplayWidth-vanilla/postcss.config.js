21ac7b1a5544bb3e79749a426317f18a4899de26
wswmsword <cjzjzzz@gmail.com>
2023-08-11 22:06:21 +0800

    update example

--- example/others/maxDisplayWidth-vanilla/postcss.config.js
+++ example/others/maxDisplayWidth-vanilla/postcss.config.js
@@ -2,10 +2,7 @@ module.exports = {
   plugins: [
     ["postcss-mobile-forever", {
       viewportWidth: 750,
-      enableMediaQuery: true,
       maxDisplayWidth: 520,
-      disableLandscape: true,
-      disableDesktop: true,
       appSelector: "#root",
       side: {
         selector1: ".footer",
